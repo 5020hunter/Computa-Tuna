b6ef950e1e68b8577dc72c196a98fa565b38401e
Grant Hunter <grant.hunter@students.fhu.edu>
2021-11-16 12:04:57 -0600

    adding comments

--- note_recognition.py
+++ note_recognition.py
@@ -9,13 +9,13 @@ samplerate, data = wavfile.read(wav_fname)
 n = len(data)
 yf = np.fft.fft(data)
 xf = np.linspace(0, samplerate//2, n//2)
-max_y = np.argmax(2.0/n * np.abs(yf[:n//2]))
-max_x = xf[max_y]
+max_y = np.argmax(2.0/n * np.abs(yf[:n//2])) #find maximum value from the set of y-values we plotted earlier
+max_x = xf[max_y] #find the x-value associated with the maximum y-value
 
 for i in range(0,72):
     note_dict = {0:"C",1:"C#/Db",2:"D",3:"D#/Eb",4:"E",5:"F",6:"F#/Gb",7:"G",8:"G#/Ab",9:"A",10:"A#/Bb",11:"B"}
-    scaling = 440*(math.pow(math.pow(2,1/12),i-57))
-    tolerance = 5
+    scaling = 440*(math.pow(math.pow(2,1/12),i-57)) #logarithmic spacing between note in reference to A4 with frequency 440Hz
+    tolerance = 5 #Hz above/below we consider in tune
     if scaling - tolerance <= max_x <= scaling + tolerance:
-        note, octave = (note_dict[i%12],i//12)
+        note, octave = (note_dict[i%12],i//12) #note is some value mod12, octave is what multiple of 12 notes we are on
         print(note, octave)
--- plot_wav.py
+++ plot_wav.py
@@ -2,30 +2,25 @@ from os.path import dirname, join as pjoin
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import wavfile
-data_dir = pjoin(dirname('output.wav'),)
+data_dir = pjoin(dirname('output.wav'),) #finding our file "output.wav" 
 wav_fname = pjoin(data_dir, 'output.wav')
-samplerate, data = wavfile.read(wav_fname)
+samplerate, data = wavfile.read(wav_fname) #taking our wav file and turning it into a list of pairs of amplitudes and times
 length = data.shape[0] / samplerate
 time = np.linspace(0, length, data.shape[0])
+#plotting the audio wave, showing amplitude over time
 plt.plot(time, data[:], label="Audio Wave")
 plt.legend()
 plt.xlabel("Time [s]")
 plt.ylabel("Amplitude")
 plt.show()
+
 n = len(data)
 yf = np.fft.fft(data)
 xf = np.linspace(0, samplerate//2, n//2)
-fig, axis = plt.subplots()
-axis.plot(xf, 2.0/n * np.abs(yf[:n//2]))
-plt.xlim(0,1000)
+#plotting the magnitudes(occurrences) of frequencies 0-1000Hz from our audio
+plt.plot(xf, 2.0/n * np.abs(yf[:n//2]))
+plt.xlim(0,1000) #0-1000Hz is the typical range for human speech
 plt.grid()
-plt.xlabel("Frequency [hZ]")
+plt.xlabel("Frequency [Hz]")
 plt.ylabel("Magnitude")
 plt.show()
-#sp=np.fft.fft(data[:, 1])
-#frequency=np.fft.fftfreq(data.size)
-#plt.plot(data[:, 1],np.abs(sp), label="Fast Fourier Transform (Our Audio)")
-#plt.legend()
-#plt.xlabel("Frequency [hZ]")
-#plt.ylabel("Amplitude")
-#plt.show()

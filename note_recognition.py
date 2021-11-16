12f6886a1694af19d90cf11228f35919d55592a0
Grant Hunter <grant.hunter@students.fhu.edu>
2021-11-16 11:35:59 -0600

    it recognizes notes!!!!!

--- note_recognition.py
+++ note_recognition.py
@@ -2,6 +2,7 @@
 from os.path import dirname, join as pjoin
 from scipy.io import wavfile
 import numpy as np
+import math
 data_dir = pjoin(dirname('output.wav'),)
 wav_fname = pjoin(data_dir, 'output.wav')
 samplerate, data = wavfile.read(wav_fname)
@@ -11,30 +12,10 @@ xf = np.linspace(0, samplerate//2, n//2)
 max_y = np.argmax(2.0/n * np.abs(yf[:n//2]))
 max_x = xf[max_y]
 
-tolerance = 0.60
-for octave in range(0,6):
-    if (octave+1)-tolerance <= max_x/16.35 <= (octave+1)+tolerance:
-        note = ("C", octave)
-    elif (octave+1)-tolerance <= max_x/17.32 <= (octave+1)+tolerance:
-        note = ("C#/Db", octave)
-    elif (octave+1)-tolerance <= max_x/18.35 <= (octave+1)+tolerance:
-        note = ("D", octave)
-    elif (octave+1)-tolerance <= max_x/19.45 <= (octave+1)+tolerance:
-        note = ("D#/Eb", octave)
-    elif (octave+1)-tolerance <= max_x/20.60 <= (octave+1)+tolerance:
-        note = ("E", octave)
-    elif (octave+1)-tolerance <= max_x/21.83 <= (octave+1)+tolerance:
-        note = ("F", octave)
-    elif (octave+1)-tolerance <= max_x/23.12 <= (octave+1)+tolerance:
-        note = ("F#/Gb", octave)
-    elif (octave+1)-tolerance <= max_x/24.50 <= (octave+1)+tolerance:
-        note = ("G", octave)
-    elif (octave+1)-tolerance <= max_x/25.96 <= (octave+1)+tolerance:
-        note = ("G#/Ab", octave)
-    elif (octave+1)-tolerance <= max_x/27.50 <= (octave+1)+tolerance:
-        note = ("A", octave)
-    elif (octave+1)-tolerance <= max_x/29.14 <= (octave+1)+tolerance:
-        note = ("A#/Bb", octave)
-    elif (octave+1)-tolerance <= max_x/30.87 <= (octave+1)+tolerance:
-        note = ("B", octave)
-print(note)
+for i in range(0,72):
+    note_dict = {0:"C",1:"C#/Db",2:"D",3:"D#/Eb",4:"E",5:"F",6:"F#/Gb",7:"G",8:"G#/Ab",9:"A",10:"A#/Bb",11:"B"}
+    scaling = 440*(math.pow(math.pow(2,1/12),i-57))
+    tolerance = 5
+    if scaling - tolerance <= max_x <= scaling + tolerance:
+        note, octave = (note_dict[i%12],i//12)
+        print(note, octave)
